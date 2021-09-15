1b6a95d3e219eb2049add3fcd3f920e15e15c15d
Azeem Zaidi <azeem.zaidi@iress.com>
2021-09-16 02:35:50 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React App on Docker by Docker-Compose! 8080
         </p>
         <a
           className="App-link"
